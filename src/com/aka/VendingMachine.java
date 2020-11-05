b31846a13c10514ed4b268936667bcb6571aa64a
Kelson Smith <sunflail2001@yahoo.com>
2020-11-05 15:41:06 -0800

    Added Comments to VendingMachine.java

--- src/com/aka/VendingMachine.java
+++ src/com/aka/VendingMachine.java
@@ -8,12 +8,27 @@ class VendingMachine {
 
     // Business methods
     public void takePayment() {
-
+        // TODO: 11/5/2020 Will take payment object from customer and store in local variable for VendingMachine 
     }
 
-    public void findItem() {}
-    public void calculateTotal() {}
-    public void dispenseSomething() {}
-    public void showMenu() {}
-    public void requestOfCustomer() {} // needs a name change
+    public void findItem() {
+        // TODO: 11/5/2020 Checks for an item based on its category and name, and also based on the menu choice  
+        //  from the Customer
+    }
+    public double calculateTotal() {
+        // TODO: 11/5/2020 Takes number of items and their prices and finds the total 
+        return 0.0;
+    }
+    public void dispenseSomething() {
+        // TODO: 11/5/2020 Checks item availability field and delivers item to customer. 
+        //  Also used to check if change is needed, in case of a cash payment, from Payment (calls makeChange())
+    }
+    public void showMenu() {
+        // TODO: 11/5/2020 Used to show any menu needed to the customer, including items, categories, total requested  
+        //  payment, balance owed/due, an item dispensed message, a thank you message, asking for anything else
+        
+    }
+    public void requestOfCustomer() {
+        // TODO: 11/5/2020 Called if VendingMachine needs to ask the customer for Money, ask for anything else 
+    } // needs a name change
 }
