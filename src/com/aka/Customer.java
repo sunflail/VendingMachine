8ea2e070ad866da74ef01a2907737a9908bf4535
Kelson Smith <sunflail2001@yahoo.com>
2020-11-05 14:54:37 -0800

    Added: com.aka package Class: Customer, Item, Menu, Payment, VendingMachine Added test folder with: CustomerTest.java

---
+++ src/com/aka/Customer.java
@@ -0,0 +1,15 @@
+package com.aka;
+
+class Customer {
+    public static void main(String[] args) {
+        // Customer instantiation and calls to VendingMachine from here
+    }
+    // Variables
+    private Enum funds; // may or may not be an Enum
+
+    // Business methods
+    public void makeRequest() {} // might be better as makeSelection()
+    public void makePayment() {}
+    public void retrieveItem() {}
+
+}
---
+++ src/com/aka/Item.java
@@ -0,0 +1,20 @@
+package com.aka;
+
+class Item {
+    // Instance Variables
+    private String name;
+    private String category; // probably reference an enum
+
+    // Accessors
+    public void getPrice() {
+
+    }
+
+    public void checkAvailability() {
+
+    }
+
+    public void updateAvailability() {
+
+    }
+}
