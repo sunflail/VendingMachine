c6c20c720f35464bf03068cb2a6aac9ac29f3dc5
Abel Miranda <available_email_address@yahoo.com>
2020-11-05 15:31:37 -0800

    Added TODOs to the Payment and Item classes

--- src/com/aka/Item.java
+++ src/com/aka/Item.java
@@ -6,14 +6,17 @@ class Item {
     private String category; // probably reference an enum
 
     // Accessors
+    // TODO: an item's price should be retrievable
     public void getPrice() {
 
     }
 
+    // TODO: check to see if the item is currently available
     public void checkAvailability() {
 
     }
 
+    // TODO: if an item is no long available, update its availability to reflect that
     public void updateAvailability() {
 
     }
--- src/com/aka/Payment.java
+++ src/com/aka/Payment.java
@@ -5,9 +5,12 @@ enum Payment {
     CASH, DEBIT_CARD, CREDIT_CARD;
 
     // Add methods
+    // TODO: check to see amount presented; if insufficient, prompt for additional payment
     void verifyBalance() {
 
     }
+
+    // TODO: compare payment amount to total cost and return the difference
     void makeChange() {
 
     }
