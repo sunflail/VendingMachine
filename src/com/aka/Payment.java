e5d9470b82709da41b4584af4a1f07b09cf281c5
Apekshya <apekshyakh@gmail.com>
2020-11-06 13:39:13 -0800

    Worked on Payment and PaymentTest

--- src/com/aka/Payment.java
+++ src/com/aka/Payment.java
@@ -1,18 +1,36 @@
 package com.aka;
 
 enum Payment {
-    // Will be an advanced enum
-    CASH, DEBIT_CARD, CREDIT_CARD;
+    //Advanced enum
+    CASH,
+    DEBIT_CARD,
+    CREDIT_CARD;
+    private static double inputAmount;
+
 
     // Add methods
     // TODO: check to see amount presented; if insufficient, prompt for additional payment
-    void verifyBalance() {
+    static boolean verifyBalance(Item item) {
+    // debit, credit input,
+        //cash - it meets the requirement if cash inputted is more than the price of the item
+        if(inputAmount>= item.getPrice()) {
+            //makeChange();
+            return true;
+        }
 
+       return false;
+    }
+/*
+    // TODO: compare payment amount to total cost and return the difference - VENDING MACHINE CLASS?
+    static double makeChange() {
+
+        double totalCost = 10;
+    return inputAmount - totalCost;
     }
 
-    // TODO: compare payment amount to total cost and return the difference
-    void makeChange() {
+ */
 
+    public static void setInputAmount(double inputAmountIn) {
+        inputAmount = inputAmountIn;
     }
-
 }
---
+++ test/com/aka/PaymentTest.java
@@ -0,0 +1,47 @@
+package com.aka;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PaymentTest {
+    private Item item1;
+    private Item item2;
+
+
+    @Before
+    public void setUp() {
+        item1 = new Item("XYZ", "Drinks", 2.99, true);
+        item2 = new Item("Tuna", "Sandwich", 1.50, true);
+    }
+
+    @Test
+    public void verifyifBalanceisEqual() {
+        Payment.setInputAmount(item1.getPrice());
+        assertEquals(true, Payment.verifyBalance(item1));
+    }
+
+    @Test
+    public void verifyifBalanceisLessthanPriceOfItem() {
+        Payment.setInputAmount(1.00);
+        assertFalse(Payment.verifyBalance(item2));
+
+
+    }
+    @Test
+    public void verifyifBalanceisGreaterthanPriceOfItem() {
+        Payment.setInputAmount(3.50);
+        assertTrue(Payment.verifyBalance(item1));
+
+    }
+/*
+    @Test
+    public void testMakeChange() {
+        Payment.setInputAmount(4.00);
+        assertEquals(-6.00,Payment.makeChange(),0.001);
+        assertEquals(-7.01,Payment.verifyBalance(item1));
+    }
+
+ */
+}
